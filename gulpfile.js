738ff5f826ce5d0b5fbfa0642bf63e29a13982c6
Anders Windelhed <anders@windelhed.se>
2015-07-07 22:32:04 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+ 
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    port: 2000,
+    livereload: true
+  });
+});
+ 
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+});
+ 
+gulp.task('default', ['connect', 'watch']);
