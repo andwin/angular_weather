1be5c4f4a7b70e7f2d35cf304dfab11552074103
Anders Windelhed <anders.windelhed@valtech.se>
2015-08-03 18:23:40 +0200

    Getting data from openweathermap.org

--- app/js/controllers/city.js
+++ app/js/controllers/city.js
@@ -1,6 +1,12 @@
 'use strict;'
 
-angular.module('myApp').controller('CityController', ['$scope', '$routeParams', function($scope, $routeParams) {
+angular.module('myApp').controller('CityController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
   $scope.name = 'CityController';
   $scope.params = $routeParams;
+
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + $routeParams.cityId;
+  $http.get(url).success(function(response) {
+    console.log(response);
+    $scope.weatherData = response;
+  });
 }]);
