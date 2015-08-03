9d2577951105be89382612fbdc5a59e385a604f9
Anders Windelhed <anders.windelhed@valtech.se>
2015-08-03 18:27:59 +0200

    Displaying google map of current city

--- app/js/controllers/city.js
+++ app/js/controllers/city.js
@@ -8,5 +8,14 @@ angular.module('myApp').controller('CityController', ['$scope', '$routeParams', 
   $http.get(url).success(function(response) {
     console.log(response);
     $scope.weatherData = response;
+
+    initializeMap(response.coord.lat, response.coord.lon);
   });
+
+  var initializeMap = function(lat, lng) {
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+      zoom: 8,
+      center: {lat: lat, lng: lng}
+    });
+  }
 }]);
