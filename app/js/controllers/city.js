008e3d6773b12da9495031f79b58e76614cbfca3
Anders Windelhed <anders.windelhed@valtech.se>
2015-08-10 21:04:30 +0200

    Using angulars date format filter

--- app/js/controllers/city.js
+++ app/js/controllers/city.js
@@ -18,13 +18,4 @@ angular.module('myApp').controller('CityController', ['$scope', '$routeParams', 
       center: {lat: lat, lng: lng},
     });
   };
-
-  $scope.formatTime = function(time) {
-    var date = new Date(time * 1000);
-    var hours = date.getHours();
-    var minutes = '0' + date.getMinutes();
-    var seconds = '0' + date.getSeconds();
-
-    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-  };
 }]);
